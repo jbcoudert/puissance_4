6af470f1223f38f15a55ff761a50534d9277684e
Lozano Elian <Elian.lozano@outlook.fr>
2019-10-07 11:29:35 +0200

    init 3

--- puissance_4.js
+++ puissance_4.js
@@ -12,43 +12,49 @@ grTablo.appendChild(tablo)
 click1 = null
 click2 = null
 
-for (let i = 0; i < 6; i++ ) {
+for (let i = 0; i < 7; i++ ) {
     var divElement = document.createElement("div")
     grDiv.appendChild(divElement)
     divElement.setAttribute("class", "divclick divclick2")
+    divElement.setAttribute("id", i+"")
+
     var trElement = document.createElement("tr")
     tablo.appendChild(trElement)
     trElement.setAttribute("class", "cell")
-        divElement.addEventListener("click", function () {
-            // if (tdElement.innerHTML == ""){
-            //     if (click1 == ) {
-                    
-            //     }
-            // }
-            if (click1 == null) {
-                var elmts = document.getElementsByClassName("divclick")
-                for (let k = 0; k < elmts.length; k++) {
-                    elmts[k].classList.remove("divclick2")
-                }
-                click1 = this
-            } 
-            else {
-                var elmts = document.getElementsByClassName("divclick")
-                for (let k = 0; k < elmts.length; k++) {
-                    elmts[k].classList.add("divclick2");
-                }
-                click2 = this
-                click1 = null
-                click2 = null
-            }
-
-        })
     
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < 7; j++) {
         var tdElement = document.createElement("td")
         trElement.appendChild(tdElement)
         tdElement.setAttribute("class", "cell")
-        // tdElement.setAttribute("id", i+ "-" +j)
+        tdElement.setAttribute("id","-" +j)
     }
+divElement.addEventListener("click", function(){
+    if (tdElement.innerHTML == "") {
+    var divduclick = document.createElement("div")
+    tdElement.appendChild(divduclick)
+    divduclick.setAttribute("class", "divclick divclick2")
+    }
+})
 }
 
+
+// divElement.addEventListener("click", function () {
+    
+//     if (click1 == null) {
+//         var elmts = document.getElementsByClassName("divclick")
+//         for (let k = 0; k < elmts.length; k++) {
+//             elmts[k].classList.remove("divclick2")
+//         }
+//         click1 = this
+//     } 
+//     else {
+//         var elmts = document.getElementsByClassName("divclick")
+//         for (let k = 0; k < elmts.length; k++) {
+//             elmts[k].classList.add("divclick2");
+//         }
+//         click2 = this
+//         click1 = null
+//         click2 = null
+//     }
+
+// })
