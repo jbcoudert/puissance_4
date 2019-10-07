accef33668252c8f4b9d062074e459eea8fc0fe7
coudert <jean.baptiste.coudert1@gmail.com>
2019-10-07 15:55:09 +0200

    finish

--- puissance_4.js
+++ puissance_4.js
@@ -12,49 +12,65 @@ grTablo.appendChild(tablo)
 click1 = null
 click2 = null
 
-for (let i = 0; i < 7; i++ ) {
+
+for (let i = 0; i < 5; i++ ) {
     var divElement = document.createElement("div")
     grDiv.appendChild(divElement)
     divElement.setAttribute("class", "divclick divclick2")
-    divElement.setAttribute("id", i+"")
+    divElement.setAttribute("id", i)
+    divElement.addEventListener("click", function(){   
+            
+        var colNum = this.id
+        
+        currentCol = document.querySelectorAll(".cell[id$='-"+colNum+"']:not(.played)")
+
+        if (currentCol.length != 0){
+        
+        lastCell = currentCol[currentCol.length -1];
+        lastCell.className = "played"
+        console.log(lastCell)
+        console.log(currentCol); 
+        var divduclick = document.createElement("div")
+        lastCell.appendChild(divduclick) 
+        divduclick.setAttribute("class", "pionRed pionYellow")
+
+        if (click1 == null) {
+            var elmts = document.getElementsByClassName("divclick")
+            for (let k = 0; k < elmts.length; k++) {
+                elmts[k].classList.remove("divclick2") 
+                divduclick.classList.add("pionYellow")
+            }
+            click1 = this
+        } 
+        else {
+            var elmts = document.getElementsByClassName("divclick")
+            for (let k = 0; k < elmts.length; k++) {
+                elmts[k].classList.add("divclick2");
+                divduclick.classList.remove("pionYellow")
+            }
+            click2 = this
+
+            click1 = null
+            click2 = null
+        }        
+        }
+        
+    })
+}
+
+
+for (let i = 0; i < 5; i++ ) {
 
     var trElement = document.createElement("tr")
     tablo.appendChild(trElement)
     trElement.setAttribute("class", "cell")
     
-    for (let j = 0; j < 7; j++) {
+    for (let j = 0; j < 5; j++) {
         var tdElement = document.createElement("td")
         trElement.appendChild(tdElement)
         tdElement.setAttribute("class", "cell")
-        tdElement.setAttribute("id","-" +j)
+        tdElement.setAttribute("id", i+"-"+j)
     }
-divElement.addEventListener("click", function(){
-    if (tdElement.innerHTML == "") {
-    var divduclick = document.createElement("div")
-    tdElement.appendChild(divduclick)
-    divduclick.setAttribute("class", "divclick divclick2")
-    }
-})
 }
 
 
-// divElement.addEventListener("click", function () {
-    
-//     if (click1 == null) {
-//         var elmts = document.getElementsByClassName("divclick")
-//         for (let k = 0; k < elmts.length; k++) {
-//             elmts[k].classList.remove("divclick2")
-//         }
-//         click1 = this
-//     } 
-//     else {
-//         var elmts = document.getElementsByClassName("divclick")
-//         for (let k = 0; k < elmts.length; k++) {
-//             elmts[k].classList.add("divclick2");
-//         }
-//         click2 = this
-//         click1 = null
-//         click2 = null
-//     }
-
-// })
